927a5994cc675fb65df25dc8fc0589c497c44700
Renilson <86357654+renilsonjss@users.noreply.github.com>
2021-08-16 23:43:52 -0300

    Create main.dart
    Arquivo main

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:sistema_de_vacinacao/Telas/tela_inicial.dart';
+
+void main() 
+{
+  runApp(MaterialApp
+  (
+    home: HomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.blue
+    ),
+  ));
+}
